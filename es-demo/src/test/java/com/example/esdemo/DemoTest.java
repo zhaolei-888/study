280b4de8b0a7f6ca66b4caf57a30e3c1a9bf37a0
zhaolei <zhaoleiyin888@163.com>
2022-08-16 03:43:09 +0800

    feat:test sortByLength

--- es-demo/src/test/java/com/example/esdemo/DemoTest.java
+++ es-demo/src/test/java/com/example/esdemo/DemoTest.java
@@ -1,11 +1,14 @@
 package com.example.esdemo;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.Set;
 
 @SpringBootTest
@@ -20,5 +23,13 @@ public class DemoTest {
         set.add(4);
         set.add(4);
         System.out.println(set);
+    }
+
+    @Test
+    public void sortByLength(){
+        List<String> list = Lists.newArrayList("he see a ball".split(" "));
+        System.out.println("排序前" + list);
+        list.sort(Comparator.comparing(String :: length));
+        System.out.println("排序后" + list);
     }
 }
--- kafka-demo/src/test/java/com/example/kafkademo/KafkaDemoApplicationTests.java
+++ kafka-demo/src/test/java/com/example/kafkademo/KafkaDemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.kafkademo;
 
+import com.google.common.collect.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @SpringBootTest
 class KafkaDemoApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
